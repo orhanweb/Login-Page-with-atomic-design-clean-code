45d65158fb2f16515f830a1d5f73c4496faca49a
Orhan-Kahraman <herorhan999@gmail.com>
2023-03-23 04:24:24 +0300

    added animated switcher experimental

--- lib/Login/animated_switcher.dart
+++ lib/Login/animated_switcher.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class AnimatedswitcherWidget extends StatefulWidget {
+  const AnimatedswitcherWidget({super.key});
   @override
+  // ignore: library_private_types_in_public_api
   _AnimatedswitcherWidgetState createState() => _AnimatedswitcherWidgetState();
 }
 
@@ -13,28 +15,23 @@ class _AnimatedswitcherWidgetState extends State<AnimatedswitcherWidget> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('AnimatedSwitcher Örneği'),
+          title: const Text('AnimatedSwitcher Örneği'),
         ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               AnimatedSwitcher(
-                duration: Duration(milliseconds: 500),
+                duration: const Duration(milliseconds: 500),
                 child: _showImage
-                    ? Image.network(
-                        'https://picsum.photos/250?image=9',
-                        key: ValueKey(1),
-                      )
-                    : Text(
-                        'Flutter',
-                        style: TextStyle(fontSize: 32),
-                        key: ValueKey(2),
-                      ),
+                    ? Image.network('https://picsum.photos/250?image=9',
+                        key: const ValueKey(1))
+                    : const Text('Flutter',
+                        style: TextStyle(fontSize: 32), key: ValueKey(2)),
               ),
-              SizedBox(height: 20),
+              const SizedBox(height: 20),
               ElevatedButton(
-                child: Text('Değiştir'),
+                child: const Text('Değiştir'),
                 onPressed: () {
                   setState(() {
                     _showImage = !_showImage;
